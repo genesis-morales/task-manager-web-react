99e47e0d10dcfa64eb9a13cb9a47cf14a1555eab
Génesis Morales <genesismoralesc1@gmail.com>
2026-03-14 15:23:50 -0600

    Refactoring folders to improve scalability.

--- src/features/auth/pages/login/Login.tsx
+++ src/features/auth/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AppNavbar from "../../../../shared/components/nav-bar/HomeNavbar";
 import AppFooter from "../../../../shared/components/footer/AppFooter";
-import LoginForm from "../../pages/login/components/LoginForm";
+import LoginForm from "../../components/login-form/LoginForm";
 import "./Login.scss";
 
 const Login: React.FC = () => (
